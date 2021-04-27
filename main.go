0d2ff7fd46453acb959a3439a316e9e8126591fe
Teodor Heggelund <teodor+git@iterate.no>
2021-04-27 16:57:29 +0200

    stuff

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import "net/http"
+import "html"
 
 func fooHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
@@ -11,9 +12,16 @@ func main() {
 	http.HandleFunc("/foo", fooHandler)
 
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-//		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
-//	log.Fatal(http.ListenAndServe(":8080", nil))
+	//	log.Fatal(http.ListenAndServe(":8080", nil))
 	http.ListenAndServe(":8080", nil)
+
+	// ben sier:
+	//  - GET
+	//  - POST
+	//
+	//  - perhaps when you post we call, send back data from external service
+	//  (like siteconfig). Something JSON?
 }
